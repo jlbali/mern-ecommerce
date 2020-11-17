b645401124a85a10abb0f45cbd67153dc1c72d53
lbali <juan.lucas.bali@gmail.com>
2020-11-17 14:28:13 -0300

    Pasaje de Params por TypeScript

--- frontend/src/screens/ProductScreen.tsx
+++ frontend/src/screens/ProductScreen.tsx
@@ -1,10 +1,28 @@
 
 import React from 'react';
+import {Link, RouteComponentProps} from 'react-router-dom';
+import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
+import Product from '../models/Product';
 
-const ProductScreen = () => {
+interface MatchParams {
+    id: string;
+}
+
+interface Props extends RouteComponentProps<MatchParams>{
+
+};
+
+const ProductScreen = (matchRoute: Props) => {
+    console.log("Route: ", matchRoute);
+    const id = matchRoute.match.params.id;
+    const product = products.find((p:Product) => p._id === id ) as Product;
+
+
     return (
         <div>
-            Product
+            {product.name}
         </div>
     );
 }
