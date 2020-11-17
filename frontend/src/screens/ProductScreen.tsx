80b7f8b02da354f93a7b0b70df1ba617b9adbac9
lbali <juan.lucas.bali@gmail.com>
2020-11-17 16:55:14 -0300

    Product Screen

--- frontend/src/screens/ProductScreen.tsx
+++ frontend/src/screens/ProductScreen.tsx
@@ -21,9 +21,65 @@ const ProductScreen = (matchRoute: Props) => {
 
 
     return (
-        <div>
-            {product.name}
-        </div>
+        <>
+            <Link className="btn btn-light my-3" to="/">
+                Volver
+            </Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>
+                            <h3>{product.name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating value={product.rating} reviews={product.numReviews} />
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Price: ${product.price}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Description: {product.description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Price:
+                                    </Col>
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Stock:
+                                    </Col>
+                                    <Col>
+                                        {product.countInStock}
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button className="btn-block" type="button" disabled={product.countInStock ==0}>
+                                    {product.countInStock > 0 ? "Agregar al Carrito": "Sin Stock"} .
+                                </Button>
+                            </ListGroup.Item>
+
+                        </ListGroup>
+                    </Card>
+                </Col>
+    
+            </Row>
+        </>
     );
 }
 
